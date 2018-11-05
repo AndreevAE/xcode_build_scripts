9c1e2c4ae3bd962d1c55eca6406eb52a14d2a725
Alexander Andreev <al101an@mail.ru>
2018-11-05 23:31:56 +0700

    Update incrementBuild.sh
    add commit, push and tag new commit with build number

--- incrementBuild.sh
+++ incrementBuild.sh
@@ -1,3 +1,6 @@
 buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
 buildNumber=$(($buildNumber + 1))
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
+git commit -m "build no. ${buildNumber}"
+git push origin develop
+git tag build_${buildNumber}
