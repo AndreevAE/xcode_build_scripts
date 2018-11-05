708c1e13b982b9b7b967a113c640bd87b323ed71
Alexander Andreev <al101an@mail.ru>
2018-11-05 23:36:37 +0700

    Update git_log.sh
    add condition on slack pre-installed, some fixes in script

--- git_log.sh
+++ git_log.sh
@@ -1,32 +1,26 @@
-# buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
-# take current build number from Project's Info.plist
-buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "Betolimp/Info.plist")
 
-# take commit's hash by tag with current build number
-commit=$(git rev-list -n 1 build_"${buildNumber}")
+slack
+rc=$?
+# check slack-cli pre-installed
+if [[ $rc == 1 ]] then
+	# take current build number from Project's Info.plist
+	buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
 
-# create new change.log and write here git logs from commit
-# change path to change.log file
-touch "/Users/$(whoami)/Desktop/changelogs/build_${buildNumber}_change.log"
-git log --pretty=format:"%h - %an, %ar : %s" ${commit}.. > "/Users/$(whoami)/Desktop/changelogs/build_${buildNumber}_change.log"
-# git log --pretty=fuller ${commit}.. > "/Users/alexander/Desktop/changelogs/build_${buildNumber}_change.log"
+	# take commit's hash by tag with current build number
+	commit=$(git rev-list -n 1 build_"${buildNumber}")
 
-# slack file upload setup
-# https://github.com/rockymadden/slack-cli
-# export SLACK_CLI_TOKEN='token'
-# slack chat send [<text> [channel]]
-# slack file upload [<file> [channels]]
+	# create new change.log and write here git logs from commit
+	# change path to change.log file
+	touch "./build_${buildNumber}_change.log"
+	git log --pretty=format:"%h - %an, %ar : %s" ${commit}.. > "./build_${buildNumber}_change.log"
+	# git log --pretty=fuller ${commit}.. > "/Users/alexander/Desktop/changelogs/build_${buildNumber}_change.log"
 
-slack file upload --file "/Users/$(whoami)/Desktop/changelogs/build_${buildNumber}_change.log" --channels [channels] --comment "build_${buildNumber}"
-rm "/Users/$(whoami)/Desktop/changelogs/build_${buildNumber}_change.log"
+	# slack file upload setup
+	# for slack-cli install https://github.com/rockymadden/slack-cli
+	export SLACK_CLI_TOKEN=[token]
+	# slack chat send [<text> [channel]]
+	# slack file upload [<file> [channels]]
 
-# increment build number
-buildNumber=$(($buildNumber + 1))
-
-# create new tag with incremented build number
-git tag build_${buildNumber}
-
-# move next 2 lines to "Increment Build script"
-# commit incremented build number in Info.plist
-# git commit -m "build no. ${buildNumber}"
-# git push origin HEAD
+	slack file upload --file "./build_${buildNumber}_change.log" --channels [channels] --comment "build_${buildNumber}"
+	rm "./build_${buildNumber}_change.log"
+fi
